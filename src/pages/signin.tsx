343e1360fa252c02eca3ba6c168f6852d5c4ae9d
Ananyobrata Pal <ananyo141@gmail.com>
2023-04-02 20:28:38 +0530

    make header responsive for verification page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -16,13 +16,13 @@ const SignIn = (props: Props) => {
             Welcome to
             <br /> Systempackage
           </h2>
-          <div className="rounded-md flex justify-between gap-5 bg-gray-100">
+          <div className="rounded-lg flex justify-between gap-5 bg-gray-100">
             <input
               type="text"
               placeholder="Username"
-              className="p-2 rounded-2xl bg-inherit"
+              className="p-2 rounded-lg bg-inherit"
             />
-            <select className="p-2 text-gray-500 rounded-md bg-white px-3 m-1">
+            <select className="p-2 text-gray-500 rounded-lg bg-white px-3 m-1">
               <option value="1">@heads.design</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -31,13 +31,13 @@ const SignIn = (props: Props) => {
           <input
             type="password"
             placeholder="********"
-            className="p-2 w-full rounded-md bg-gray-100"
+            className="p-3 w-full rounded-lg bg-gray-100"
           />
           <div className="relative inline">
             <RxEyeNone className="absolute top-1 opacity-75 right-5" />
           </div>
           <Link className="block" href="/verification">
-            <button className="w-full text-xl h-16 font-bold tracking-wide rounded-md bg-blue-600 text-white">
+            <button className="w-full text-xl h-16 font-bold tracking-wide rounded-lg bg-blue-600 text-white">
               Next
             </button>
           </Link>
@@ -45,10 +45,18 @@ const SignIn = (props: Props) => {
             Forgot your password?
           </p>
           <p className="font-light">
-            Not member? <span className="font-bold text-blue-600 cursor-pointer"> Create account</span>
+            Not member?{" "}
+            <span className="font-bold text-blue-600 cursor-pointer">
+              {" "}
+              Create account
+            </span>
           </p>
         </div>
-        <Image src={cardImage} alt="Card Image" />
+        <Image
+          className="hidden lg:block w-1/2"
+          src={cardImage}
+          alt="Card Image"
+        />
       </div>
     </section>
   );
